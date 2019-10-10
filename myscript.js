a1b3eb3193a305ce62b9086369ec7e2d12cef863
624167 <624167@pdsb.net>
2019-10-10 09:26:03 -0400

    l

---
+++ myscript.js
@@ -0,0 +1,9 @@
+
+function myFunction() {
+  let a1= parseInt(prompt("Enter first angle"));
+  alert("Hiii");
+  let a2=parseInt(prompt("Enter second angle"));
+  let a3 = 180 - (a1 + a2);
+  alert("angle 3 is"+ a3);
+}
+
