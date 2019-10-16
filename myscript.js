8c1b411e2c8fba824fda798b118227791279dfed
624167 <624167@pdsb.net>
2019-10-16 09:31:41 -0400

    Yayyy

--- myscript.js
+++ myscript.js
@@ -4,15 +4,19 @@ function myFunction() {
  
   let a2=parseInt(prompt("Enter second angle"));
   let a3 = 180 - (a1 + a2);
-  alert("angle 3 is"+ " "+a3);
+  alert("angle 3 is"+ " "+ a3);
 }
 function century(){
 let year1= parseInt(prompt("Enter a year"));
 
 
-let century1=parseInt((year - 1) / 100)+ 1;
-alert("The century is"+ century1);
+let century=parseInt((year1 - 1) / 100)+ 1;
+alert("The century is"+ century);
 
 
 }
 
+function integer(){
+
+
+}
