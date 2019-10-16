d32139343a1f5efa781dcd8dd3ed5675d01cb08e
624167 <624167@pdsb.net>
2019-10-16 09:23:48 -0400

    New Button

--- myscript.js
+++ myscript.js
@@ -1,9 +1,18 @@
 
 function myFunction() {
   let a1= parseInt(prompt("Enter first angle"));
-  alert("Hiii");
+ 
   let a2=parseInt(prompt("Enter second angle"));
   let a3 = 180 - (a1 + a2);
   alert("angle 3 is"+ a3);
+}
+function century(){
+let year1= parseInt(prompt("Enter a year"));
+
+
+let century1=parseInt((year - 1) / 100)+ 1;
+alert("The century is"+ century1);
+
+
 }
 
